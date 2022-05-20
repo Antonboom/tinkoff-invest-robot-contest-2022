a4a3518e848ec2b3ab9798ac13745d2054bbeb6b
Антон Телышев <anton.telishev@yandex.ru>
2022-05-20 09:12:44 +0300

    Add lost file

---
+++ cmd/trading-robot/strategies__spread_mon.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog/log"
+
+	"github.com/Antonboom/tinkoff-invest-robot-contest-2022/internal/clients/tinkoffinvest"
+	"github.com/Antonboom/tinkoff-invest-robot-contest-2022/internal/config"
+)
+
+func runSpreadMonitoring(
+	ctx context.Context,
+	account string,
+	cfg config.SpreadMonitoringConfig,
+	client *tinkoffinvest.Client,
+) error {
+	tools, err := client.GetFullAvailableInstruments(ctx)
+	if err != nil {
+		return fmt.Errorf("get available instruments: %v", err)
+	}
+
+	filteredTools := make([]tinkoffinvest.Instrument, 0, len(tools))
+	for _, t := range tools {
+		logger := log.With().Str("figi", t.FIGI).Logger()
+
+		orderBook, err := client.GetOrderBook(ctx, tinkoffinvest.OrderBookRequest{
+			FIGI:  t.FIGI,
+			Depth: cfg.Depth,
+		})
+		if err != nil {
+			logger.Err(err).Msg("get order book")
+			continue
+		}
+
+		spread := tinkoffinvest.Spread(*orderBook)
+
+		logger.Debug().
+			Str("name", t.Name).
+			Str("limit_up", orderBook.LimitUp.String()).
+			Str("limit_down", orderBook.LimitDown.String()).
+			Float64("spread", spread).
+			Msg("spread")
+
+		if spread >= cfg.MinSpreadPercentage {
+			filteredTools = append(filteredTools, t)
+		}
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case <-time.After(800 * time.Millisecond):
+		}
+	}
+
+	json.NewEncoder(os.Stdout).Encode(filteredTools)
+
+	return nil
+}
